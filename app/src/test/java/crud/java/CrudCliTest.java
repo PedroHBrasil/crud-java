66d378f923e7940a4f40dd2658bfc0b6c05b52da
PedroHBrasil <pedrohbrasil7@gmail.com>
2023-08-24 21:30:27 -0300

    clenned crud cli test code

--- app/src/test/java/crud/java/CrudCliTest.java
+++ app/src/test/java/crud/java/CrudCliTest.java
@@ -7,19 +7,14 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.Scanner;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CrudCliTest {
 
     private final InputStream originalIn = System.in;
     private final PrintStream originalOut = System.out;
-    // @BeforeEach
-    // public void setUpStreams() {
-    // }
 
     @AfterEach
     public void restoreStreams() {
